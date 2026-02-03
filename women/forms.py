3dd0088b6c8a1711ba3acda6b2ad29fa92ebbf39
Pavel_Doroshenko <grelkin07@mail.ru>
2026-02-03 22:52:08 +0700

    first commit

---
+++ women/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.utils.deconstruct import deconstructible
+#from prompt_toolkit.validation import ValidationError
+
+from .models import Category, Husband, Women
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MaxLengthValidator, MinLengthValidator
+
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+
+    def __call__(self, value):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code, params={"value": value})
+
+class AddPostForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории', empty_label='Категория не выбрана')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж', empty_label='Не замужем')
+
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols':50, 'rows': 10}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError('Длинна превышает 50 символов')
+
+        return title
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label='Файл')
